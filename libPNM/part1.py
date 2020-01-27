43c7d105484385cad718dfda7258a47c0c27ccda
Arvin Lin <al4419@graphic03.doc.ic.ac.uk>
2020-01-27 21:15:27 +0000

    version 1

---
+++ libPNM/part1.py
@@ -0,0 +1,88 @@
+import sys
+import numpy as np
+from PNM import *
+
+import matplotlib.colors as colors
+import matplotlib.pyplot as plt
+
+# Step 4. Design a center weighting function used in Step 4.
+def w_function(x):
+	return 16*x**2*(1-x)**2
+
+# Implement a tone mapper
+def tone_map(F, stops = 0, gamma = 1.0):
+	# x should be an hsv image
+	# scales with exposure
+	x = F.copy()
+	x[:,:,2] *= 2**stops
+	# Clip
+	x[x[:,:,2] > 1, 2] = 1
+	x[:,:,2] **= (1/gamma) 
+	return x
+
+
+if '__main__' == __name__:
+	num_of_img = 7
+	# Step 1. Load all images into a list
+	Office = []
+	for i in range(num_of_img):
+		filename = '../Office/Office'+str(i+1)+'.pfm'
+		img_ = loadPFM(filename)
+		# Step 1-1. Convert images from RGB to HSV
+		img_ = colors.rgb_to_hsv(img_)
+		Office.append(img_)
+
+	# Step 2. Create a blank image buffer F
+	F = np.zeros(shape=Office[0].shape, dtype=np.float32)
+	# Step 3. Eliminate bad values
+	for i in range(num_of_img):
+		img = Office[i]
+		img[img[:,:,2] > 0.92, 2] = 0 #Set value to zero
+		img[img[:,:,2] < 0.005, 2] = 0
+		Office[i] = img
+
+	# Step 4. Implement a center weighting function
+	w = w_function
+	# Step 5. Compute F(x,y)
+	# Step 5-1. Compute w(Z(x,y)) and the sum of weights, update Z(x,y)
+	sum_w = np.zeros(shape=(F.shape[0], F.shape[1], 1), dtype = np.float32)
+	for i, img in enumerate(Office):
+		weight = np.expand_dims(w(img[:,:,2]), axis = 2)
+		img[:,:,2] /= 4**(i)
+		F += np.log(img + 1e-10)*weight #plus 1e-10 for numerical stability
+		sum_w += weight
+
+	# Step 5-2. Divide by sum of weights(normalize) and take exponential
+	F = np.exp(F/(sum_w+1e-10))
+	# Save as pfm
+	F_rgb = colors.hsv_to_rgb(F)
+	writePFM('part1img/output_results.pfm',F_rgb)
+	# print out the dynamic range
+	print('max brightness - min brightness '+str(F[:,:,2].max()-F[:,:,2].min()))
+	# Here we visualize the HDR image with various tone mappers
+	save_params = [(5,1), (6,1), (7,1),(8,1),(9,1),(10,1),(11,1),(12,1),(5,3.0),(6,3.0),(7,2.5),(7,3.0),(8,2.2),(8,2.5)]
+	stops = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+	for s in stops:
+		# Here we assume gamma = 1
+		g = tone_map(F, stops = s)
+		g_rgb = colors.hsv_to_rgb(g)
+		g_rgb = (g_rgb*255.0).astype('uint8')
+		plt.imshow(g_rgb)
+		plt.title('+'+str(s)+' stops, gamma = 1')
+		plt.show()
+		if (s, 1) in save_params:
+			filename = 'part1img/stops_'+str(s)+'_gamma_1.ppm'
+			writePPM(filename, g_rgb)
+	stops = [5,6,7,8,9]
+	gammas = [1.5, 1.8, 2.2, 2.5, 3.0]
+	for s in stops:
+		for gamma in gammas:
+			g = tone_map(F, stops = s, gamma = gamma)
+			g_rgb = colors.hsv_to_rgb(g)
+			g_rgb = (g_rgb*255.0).astype('uint8')
+			plt.imshow(g_rgb)
+			plt.title('+'+str(s)+' stops, gamma = '+str(gamma))
+			plt.show()
+			if (s, gamma) in save_params:
+				filename = 'part1img/stops_'+str(s)+'_gamma_'+str(gamma)+'.ppm'
+				writePPM(filename, g_rgb)
---
+++ libPNM/part2.py
@@ -0,0 +1,67 @@
+import sys
+import numpy as np
+from PNM import *
+
+import matplotlib.colors as colors
+
+# Gamma function
+def gamma(F, gamma = 1.0):
+	# x should be an hsv image
+	# scales with exposure
+	x = colors.rgb_to_hsv(F)
+	x[:,:,2] **= (1/gamma)
+	x = colors.hsv_to_rgb(x)
+	return x
+
+if '__main__' == __name__:
+	# Create a blank image of shape 511x511x3
+	img = np.zeros(shape=(511, 511, 3), dtype=np.float32)
+	# Create blank holders for n image and r image
+	n_img = np.zeros(shape=(511, 511, 3), dtype = np.float32)
+	r_img = np.zeros(shape=(511, 511, 3), dtype = np.float32)
+	# Load the lat long map
+	map_ =  loadPFM('../UrbanProbe/urbanEM_latlong.pfm')
+	# Define the center of image, radius of the mirror ball, and the veiw vector
+        center_pixel = (511-1)/2
+	radius = 511.0/2
+	v = np.array([0,0,1])
+
+
+	map_hx = map_.shape[1]
+	map_hy = map_.shape[0]
+	for i in range(511):
+		for j in range(511):
+			# check if the current pixel lies within the circle
+			if np.sqrt((i-center_pixel)**2+(j-center_pixel)**2) < radius:
+				#img[i,j,:] = np.array([1,1,1]) #for checking purpose
+				#define the normal vector for this pixel
+				y = (center_pixel - i)/radius; x = -(center_pixel-j)/radius
+				z_comp = np.sqrt(1-(x**2+y**2))
+		                n = np.array([x, y, z_comp])
+				# calculate r given n and v
+				r = 2*np.dot(n,v)*n-v
+				# store n and r in the blank images
+				n_img[i,j,:] = n
+				r_img[i,j,:] = r
+				# now find the index of the latlong img
+				theta = np.arccos(r[1])
+				phi = np.arctan2(r[0],r[2]) + np.pi
+				index_1 = min(int(phi/(2*np.pi)*map_hx), map_.shape[1]-1) #clip the value
+				index_0 = min(int(theta/np.pi*map_hy), map_.shape[0]-1)
+				pixel_val = map_[index_0, index_1,:]
+				pixel_val[pixel_val > 1] = 1 #clip the value
+				img[i,j,:] = pixel_val
+	# write out the n images and the r images
+	writePFM('part2img/n_image.pfm',n_img)
+	writePFM('part2img/r_image.pfm',r_img)
+	writePPM('part2img/n_image.ppm',((n_img+1)*127.5).astype('uint8')) ##but background is not black
+	writePPM('part2img/r_image.ppm',((r_img+1)*127.5).astype('uint8'))
+	# write out the mirror ball image
+	writePFM('part2img/mirror_ball.pfm',img)
+	writePPM('part2img/mirror_ball.ppm',(img*225).astype('uint8'))
+	# apply different value of gamma
+	gammas = [1.5, 1.8, 2.2, 2.5, 3.0]
+	for g in gammas:
+		img_gamma = gamma(img, gamma = g)
+		filename = 'part2img/mirror_ball_gamma'+str(g)
+		writePPM(filename+'.ppm', (img_gamma*255).astype('uint8'))
